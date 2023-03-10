6d662f3a00060a642df1a6b3c7c6e9f366205829
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-03-10 11:25:45 +0000

    added helmet middleware

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const helmet = require("helmet");
 const routes = require("./routes/routes");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
