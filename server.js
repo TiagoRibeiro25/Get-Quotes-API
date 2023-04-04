6c9f73f8298353e8073638d209179cfef2dfba5a
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-04-05 00:19:21 +0100

    Added Morgan
    Added Morgan npm package: https://www.npmjs.com/package/morgan

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
+const morgan = require("morgan");
 const routes = require("./routes/routes");
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
