c70819edab49bd45634d9346878f04076d4e8619
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-04-11 18:56:23 +0100

    Improved error handling
    Improved error handling on the quotes controllers

--- controllers/quotes.controllers.js
+++ controllers/quotes.controllers.js
@@ -1,16 +1,12 @@
 const quotesDB = require("../database/QuotesDB");
 
-function handleError(error, res) {
-	res.status(500).json({ error: "Internal server error" });
-}
-
 async function getQuotes(req, res) {
 	try {
 		await quotesDB.connect();
 		const quotes = await quotesDB.getQuotes();
 		res.status(200).json(quotes);
 	} catch (error) {
-		handleError(error, res);
+		res.status(500).json({ error: "Internal server error" });
 	} finally {
 		await quotesDB.disconnect();
 	}
@@ -22,7 +18,7 @@ async function getRandomQuote(req, res) {
 		const randomQuote = await quotesDB.getRandomQuote();
 		res.status(200).json(randomQuote);
 	} catch (error) {
-		handleError(error, res);
+		res.status(500).json({ error: "Internal server error" });
 	} finally {
 		await quotesDB.disconnect();
 	}
@@ -33,12 +29,16 @@ async function addQuote(req, res) {
 		return res.status(401).json({ error: "Unauthorized" });
 	}
 
+	if (!req.body.author || !req.body.content) {
+		return res.status(400).json({ error: "Missing author or content" });
+	}
+
 	try {
 		await quotesDB.connect();
 		await quotesDB.addQuote({ author: req.body.author, content: req.body.content });
 		res.status(200).json({ message: "Quote added successfully" });
 	} catch (error) {
-		handleError(error, res);
+		res.status(500).json({ error: "Internal server error" });
 	} finally {
 		await quotesDB.disconnect();
 	}
@@ -51,11 +51,15 @@ async function deleteQuote(req, res) {
 
 	try {
 		await quotesDB.connect();
-		const result = await quotesDB.deleteQuote(req.params.id);
-		if (!result) return res.status(404).json({ error: "Quote not found" });
-		else res.status(204).json({ message: "Quote deleted successfully" });
+		await quotesDB.deleteQuote(req.params.id);
+		res.status(204).json({ message: "Quote deleted successfully" });
 	} catch (error) {
-		handleError(error, res);
+		console.log(error.name);
+
+		// if the error is a MongoError, then the id is valid but the quote is not found
+		if (error.name === "BSONError") res.status(404).json({ error: "Quote not found" });
+		// if the error is a different error, then it's an internal server error
+		else res.status(500).json({ error: "Internal server error" });
 	} finally {
 		await quotesDB.disconnect();
 	}
