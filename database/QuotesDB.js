c3dc315551bb71ba642f342e3f8cda12fb2e313a
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-04-12 22:14:06 +0100

    Updated QuotesDB class
    Changed uri and client to private variables

--- database/QuotesDB.js
+++ database/QuotesDB.js
@@ -2,48 +2,51 @@ const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 class QuotesDB {
+	#uri;
+	#client;
+
 	constructor(username, password, uri) {
-		this.uri = `mongodb+srv://${username}:${password}@${uri}/?retryWrites=true&w=majority`;
-		this.client = new MongoClient(this.uri, {
+		this.#uri = `mongodb+srv://${username}:${password}@${uri}/?retryWrites=true&w=majority`;
+		this.#client = new MongoClient(this.#uri, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 		});
 	}
 
 	async connect() {
-		await this.client.connect();
+		await this.#client.connect();
 	}
 
 	async disconnect() {
 		try {
-			await this.client.close();
+			await this.#client.close();
 		} catch (error) {
 			throw new Error("Error disconnecting from MongoDB");
 		}
 	}
 
 	async getQuotes() {
-		const db = this.client.db(process.env.DB_NAME);
+		const db = this.#client.db(process.env.DB_NAME);
 		const collection = db.collection("quotes");
 		const quotes = await collection.find().toArray();
 		return quotes;
 	}
 
 	async getRandomQuote() {
-		const db = this.client.db(process.env.DB_NAME);
+		const db = this.#client.db(process.env.DB_NAME);
 		const collection = db.collection("quotes");
 		const randomQuote = await collection.aggregate([{ $sample: { size: 1 } }]).toArray();
 		return randomQuote[0];
 	}
 
 	async addQuote(quote) {
-		const db = this.client.db(process.env.DB_NAME);
+		const db = this.#client.db(process.env.DB_NAME);
 		const collection = db.collection("quotes");
 		await collection.insertOne(quote);
 	}
 
 	async deleteQuote(id) {
-		const db = this.client.db(process.env.DB_NAME);
+		const db = this.#client.db(process.env.DB_NAME);
 		const collection = db.collection("quotes");
 		const result = await collection.deleteOne({ _id: new mongodb.ObjectId(id) });
 		return !result.deletedCount == 0;
